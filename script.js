0193534332ebdad8d85633aa5bcc922daab9b16a
Rishabhjain2003 <99461810+Rishabhjain2003@users.noreply.github.com>
2023-08-19 00:17:01 +0530

    19/8/23

--- script.js
+++ script.js
@@ -14,3 +14,42 @@ tabs.forEach(tab => {
     target.classList.add('active')
   })
 })
+
+let t = document.getElementById('typewriter');
+const arr = ["Rishabh Jain. ", "Developer. ", "Programmer. "];
+let i = 0;
+let j = 1;
+let chk = true;
+const typ = () =>{
+  if(j==arr[i].length){
+    chk = false
+  }
+  if(j==0 && chk==false){
+    i++;
+    chk = true;
+    j = 1;
+  }
+  if(i>=3){
+    i = 0;
+  }
+  let s = arr[i].slice(0,j);
+  (chk == true) ? j++ : j--;
+  t.innerText = s;
+  setTimeout(()=>typ(), 200);
+}
+typ();
+
+
+// for(var i=0;i<arr.length;i=i+1){
+//   var s = "";
+//   for(var j=0;j<arr[i].length;j=j+1){
+//     setTimeout(() => {  console.log("Hello"); }, 50000);
+//     s += arr[i][j];
+//     t.innerText = s;
+//   }
+//   for(var j=arr[i].length-1;j>=1;j--){
+//     setTimeout(() => {  console.log(s); }, 50000);
+//     s = s.substring(0,j);
+//     t.innerText = s;
+//   }
+// }
